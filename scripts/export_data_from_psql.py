3247736923a81d1ad6d7f67b5c95ff3f5f5aae68
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2023-09-21 15:31:39 +0800

    fix bug

--- scripts/export_data_from_psql.py
+++ scripts/export_data_from_psql.py
@@ -19,6 +19,16 @@ import numpy as np
 from datetime import datetime, timedelta
 import sqlalchemy as sa
 
+
+with db.begin() as conn:
+    qry = sa.text("select * from taxon")
+    resultset = conn.execute(qry)
+    taxon = resultset.mappings().all()
+
+taxon = pd.DataFrame(taxon)
+taxon = taxon.drop(columns=['scientificNameID','id'])
+
+
 group_list = ['brcas','brmas','cpami','fact','forest','ntm','oca','taif','tcd','tbri','wra'] 
 # gbif 另外處理
 
@@ -28,7 +38,7 @@ group_list = ['brcas','brmas','cpami','fact','forest','ntm','oca','taif','tcd','
 # parentTaxonID
 # null
 
-group_list = ['ntm']
+# group_list = ['ntm']
 
 # taxonID
 for group in group_list:
@@ -37,71 +47,24 @@ for group in group_list:
     has_more_data = True
     while has_more_data:
         results = []
-        print('taxonID ', group, offset)
         with db.begin() as conn:
-            qry = sa.text("""WITH pre_select AS (select * from records  
-                          where "group" = '{}' and "taxonID" is not null limit {} offset {} )
-                          SELECT * FROM pre_select
-                         left join taxon on pre_select."taxonID" = taxon."taxonID" """.format(group, limit, offset)) # 以防萬一還是寫left
+            qry = sa.text("""select * from records  
+                          where "group" = '{}' limit {} offset {}  """.format(group, limit, offset)) 
             resultset = conn.execute(qry)
             results = resultset.mappings().all()
         if len(results):
-            now = datetime.now()
             df = pd.DataFrame(results)
             df = df.drop(columns=['id'])
             df = df.rename(columns={'tbiaID': 'id'})
-            df.to_csv(f'/solr/csvs/updated/{group}_t_{offset}.csv', index=None)
-            offset += limit
-        if len(results) < limit:
-            has_more_data = False
-
- 
-for group in group_list:
-    limit = 10000
-    offset = 0
-    has_more_data = True
-    while has_more_data:
-        print('parentTaxonID ', group, offset)
-        with db.begin() as conn:
-            # qry = sa.text("""select * from records 
-            #             join taxon on taxon."taxonID" = records."parentTaxonID"
-            #             where records."group" = '{}' 
-            #             limit {} offset {}""".format(group, limit, offset))
-            qry = sa.text("""WITH pre_select AS (select * from records where "group" = '{}' and "parentTaxonID" is not null limit {} offset {} )
-                          SELECT * FROM pre_select
-                          left join taxon on pre_select."parentTaxonID" = taxon."taxonID" """.format(group, limit, offset)) # 以防萬一還是寫left
-            resultset = conn.execute(qry)
-            results = resultset.mappings().all()
-        if len(results):
-            now = datetime.now()
-            df = pd.DataFrame(results)
-            df = df.drop(columns=['id'])
-            df = df.rename(columns={'tbiaID': 'id'})
-            df.to_csv(f'/solr/csvs/updated/{group}_pt_{offset}.csv', index=None)
-            offset += limit
-        if len(results) < limit:
-            has_more_data = False
-
- 
-for group in group_list:
-    limit = 10000
-    offset = 0
-    has_more_data = True
-    while has_more_data:
-        print('no taxonID ', group, offset)
-        with db.begin() as conn:
-            qry = sa.text("""select * from records 
-                        where records."group" = '{}' and records."parentTaxonID" is null and records."taxonID" is null
-                        limit {} offset {}""".format(group, limit, offset))
-            resultset = conn.execute(qry)
-            results = resultset.mappings().all()
-        if len(results):
-            now = datetime.now()
-            print(len(results))
-            df = pd.DataFrame(results)
-            df = df.drop(columns=['id'])
-            df = df.rename(columns={'tbiaID': 'id'})
-            df.to_csv(f'/solr/csvs/updated/{group}_nt_{offset}.csv', index=None)
+            # taxonID
+            a = df[df.taxonID.notnull()].merge(taxon,on='taxonID')
+            # parentTaxonID
+            b = df[df.parentTaxonID.notnull()].drop(columns=['taxonID']).merge(taxon, left_on='parentTaxonID', right_on='taxonID')
+            b['taxonID'] = None
+            # null
+            c = df[(df.taxonID.isnull()&df.parentTaxonID.isnull())]
+            final_df = pd.concat([a,b,c],ignore_index=True)
+            final_df.to_csv(f'/solr/csvs/updated/{group}_{offset}.csv', index=None)
             offset += limit
         if len(results) < limit:
             has_more_data = False
