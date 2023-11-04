469612cc05429072704668359cb25ca77a940f81
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2023-11-04 11:25:06 +0800

    update

--- scripts/export/export_data_from_psql.py
+++ scripts/export/export_data_from_psql.py
@@ -1,5 +1,9 @@
 # 重新比對的時候要抓回原始檔
 
+rights_list = ["台灣生物多樣性網絡 TBN", "生態調查資料庫系統","臺灣國家公園生物多樣性資料庫","臺灣生物多樣性資訊機構 TaiBIF","林業試驗所植物標本資料庫","濕地環境資料庫","林業試驗所昆蟲標本館",
+ "河川環境資料庫","中央研究院生物多樣性中心植物標本資料庫","國立臺灣博物館典藏","海洋保育資料倉儲系統","GBIF"]
+
+
 import requests
 import pandas as pd
 
@@ -38,12 +42,15 @@ taxon = taxon.drop(columns=['scientificNameID','id'])
 # parentTaxonID
 # null
 
-group_list = ['nps']
+# group_list = ['nps']
 import time
 
 # taxonID
+r_index = 0
 
-for group in group_list:
+for r in rights_list:
+    r_count = 0
+    r_index += 1
     limit = 10000
     offset = 0
     min_id = 1
@@ -53,11 +60,12 @@ for group in group_list:
         results = []
         with db.begin() as conn:
             qry = sa.text("""select * from records  
-                          where "group" = '{}' AND id > {} order by id limit {}  """.format(group, min_id, limit)) 
+                          where "rightsHolder" = '{}' AND id > {} order by id limit {}  """.format(r, min_id, limit)) 
             resultset = conn.execute(qry)
             results = resultset.mappings().all()
-        print(time.time()-s, group, offset, min_id)        
+        print(time.time()-s, r, offset, min_id)        
         if len(results):
+            r_count += len(results)
             df = pd.DataFrame(results)
             # 下一次query最小的id
             min_id = df.id.max()
@@ -71,7 +79,8 @@ for group in group_list:
             # null
             c = df[(df.taxonID.isnull()&df.parentTaxonID.isnull())]
             final_df = pd.concat([a,b,c],ignore_index=True)
-            final_df.to_csv(f'/solr/csvs/updated/{group}_{offset}.csv', index=None)
+            final_df.to_csv(f'/solr/csvs/export/{r_index}_{offset}.csv', index=None)
             offset += limit
         if len(results) < limit:
             has_more_data = False
+    print(r, r_count)
