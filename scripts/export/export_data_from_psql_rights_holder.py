41ab5c7ba5ff2cecc1bc218df86917b567699ec6
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2023-12-05 13:17:46 +0800

    update export script

--- scripts/export/export_data_from_psql_rights_holder.py
+++ scripts/export/export_data_from_psql_rights_holder.py
@@ -6,104 +6,67 @@ import pandas as pd
 from numpy import nan
 from app import db
 
-# 2023-09-04 重新比對學名
 import pandas as pd
 
-from app import portal_db_settings
 # 取得taxon資料
-import psycopg2
 import requests
 import re
-import urllib
 import numpy as np
 from datetime import datetime, timedelta
 import sqlalchemy as sa
 
 from scripts.utils import get_taxon_df
-# with db.begin() as conn:
-#     qry = sa.text("select * from taxon")
-#     resultset = conn.execute(qry)
-#     taxon = resultset.mappings().all()
-
-# taxon = pd.DataFrame(taxon)
-# taxon = taxon.drop(columns=['scientificNameID','id'])
-
-
-# url = "http://solr:8983/solr/taxa/select?indent=true&q.op=OR&q=*%3A*&rows=2147483647"
-# resp = requests.get(url)
-# taxon = resp.json()['response']['docs']
-
-
-# with db.begin() as conn:
-#     qry = sa.text("select * from taxon")
-#     resultset = conn.execute(qry)
-#     taxon = resultset.mappings().all()
-
-# taxon = pd.DataFrame(taxon)
-# taxon = taxon.rename(columns={'id': 'taxonID'})
-# taxon = taxon.drop(columns=['taxon_name_id','_version_'])
-# taxon = taxon.replace({nan:None})
-
-
-# group_list = ['brcas','brmas','cpami','fact','forest','ntm','oca','taif','tcd','wra'] 
-# gbif tbri 另外處理
-
-# 要join taxon的表
-
-# taxonID
-# parentTaxonID
-# null
-
 import time
 
-# taxonID
 
-# for group in group_list:
+r_list = ['GBIF',
+'中央研究院生物多樣性中心植物標本資料庫',
+'台灣生物多樣性網絡 TBN',
+'國立臺灣博物館典藏',
+'林業試驗所昆蟲標本館',
+'林業試驗所植物標本資料庫',
+'河川環境資料庫',
+'濕地環境資料庫',
+'生態調查資料庫系統',
+'臺灣國家公園生物多樣性資料庫',
+'臺灣生物多樣性資訊機構 TaiBIF',
+'海洋保育資料倉儲系統']
 
-total_count = 0
+r_index = 0
 
-limit = 10000
-offset = 0
-min_id = 1
-has_more_data = True
-rights_holder = ''
-group = ''
-group_index = 0
-
-while has_more_data:
-    s = time.time()
-    results = []
-    with db.begin() as conn:
-        qry = sa.text("""select * from records  
-                        where "rightsHolder" = '{}' id > {} order by id limit {}  """.format(rights_holder, min_id, limit)) 
-        resultset = conn.execute(qry)
-        results = resultset.mappings().all()
-    print(time.time()-s, offset, min_id)        
-    if len(results):
-        total_count += len(results)
-        df = pd.DataFrame(results)
-        # 下一次query最小的id
-        min_id = df.id.max()
-        df = df.drop(columns=['id'])
-        df = df.rename(columns={'tbiaID': 'id'})
-        if len(df[df.taxonID.notnull()]):
-            taxon = get_taxon_df(taxon_ids=df[df.taxonID.notnull()].taxonID.to_list())
-            # taxonID
-            final_df = df.merge(taxon,on='taxonID',how='left')
-        else:
-            final_df = df
-        # a = df[df.taxonID.notnull()].merge(taxon,on='taxonID',)
-        # parentTaxonID
-        # b = df[df.parentTaxonID.notnull()].drop(columns=['taxonID']).merge(taxon, left_on='parentTaxonID', right_on='taxonID')
-        # b['taxonID'] = None
-        # null
-        # c = df[(df.taxonID.isnull()&df.parentTaxonID.isnull())]
-        # final_df = pd.concat([a,b,c],ignore_index=True)
-        if len(results) != len(final_df):
-            print('error', min_id)
-        final_df.to_csv(f'/solr/csvs/export/{group}_{group_index}_export_{offset}.csv', index=None)
-        offset += limit
-    if len(results) < limit:
-        has_more_data = False
-
-print('total_count', total_count)
+for r in r_list:
+    total_count = 0
+    r_index += 1
+    limit = 10000
+    offset = 0
+    min_id = 1
+    has_more_data = True
+    while has_more_data:
+        s = time.time()
+        results = []
+        with db.begin() as conn:
+            qry = sa.text("""select * from records  
+                            where id > {} and "rightsHolder" = '{}' order by id limit {}  """.format(min_id, r, limit)) 
+            resultset = conn.execute(qry)
+            results = resultset.mappings().all()
+        print(time.time()-s, offset, min_id)        
+        if len(results):
+            total_count += len(results)
+            df = pd.DataFrame(results)
+            # 下一次query最小的id
+            min_id = df.id.max()
+            df = df.drop(columns=['id'])
+            df = df.rename(columns={'tbiaID': 'id'})
+            if len(df[df.taxonID.notnull()]):
+                taxon = get_taxon_df(taxon_ids=df[df.taxonID.notnull()].taxonID.to_list())
+                # taxonID
+                final_df = df.merge(taxon,on='taxonID',how='left')
+            else:
+                final_df = df
+            if len(results) != len(final_df):
+                print('error', min_id)
+            final_df.to_csv(f'/solr/csvs/export/{r_index}_export_{offset}.csv', index=None)
+            offset += limit
+        if len(results) < limit:
+            has_more_data = False
+    print(r, 'total_count', total_count)
