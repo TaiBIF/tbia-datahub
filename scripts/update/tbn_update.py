2c3e16511cb64ce31daf9d1629c67ef437942abd
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2024-01-17 10:26:34 +0800

    update

--- scripts/update/tbn_update.py
+++ scripts/update/tbn_update.py
@@ -46,23 +46,31 @@ if response.status_code == 200:
     else:
         update_version = 1
 
+# 在開始之前 先確認存不存在 
+# 若不存在 insert一個新的update_version
+current_page, note = insert_new_update_version(rights_holder=rights_holder,update_version=update_version)
+
 
 # 自產資料 + eBird
 url_list = ['https://www.tbn.org.tw/api/v25/occurrence?selfProduced=y', 'https://www.tbn.org.tw/api/v25/occurrence?datasetUUID=4fa7b334-ce0d-4e88-aaae-2e0c138d049e']
-
+# url_list = ['https://www.tbn.org.tw/api/v25/occurrence?datasetUUID=7a1d54c7-8f2e-4672-bb5b-bc29249aff9f']
 now = datetime.now() + timedelta(hours=8)
 
-url_index = 0
-for url in url_list:
-    url_index += 1
+if not note:
+    url_index = 0
+else:
+    url_index = note
+
+for url in url_list[url_index:]:
+    # print(url)
     response = requests.get(url + '&limit=1')
-    c = 0
+    # c = 0
     if response.status_code == 200:
         result = response.json()
         total_count = result['meta']['total'] 
         total_page = math.ceil(total_count/1000) # 1182
     # print(total_count, total_page)
-    for p in range(0,total_page,10):
+    for p in range(current_page,total_page,10):
         data = []
         c = p
         while c < p + 10 and c < total_page:
@@ -87,6 +95,7 @@ for url in url_list:
             df = df.reset_index(drop=True)
             df = df.replace({nan: '', None: ''})
             df['locality'] = df.apply(lambda x: x.county + x.municipality, axis = 1)
+            df['locality'] = df['locality'].apply(lambda x: x.strip())
             # 若沒有individualCount 則用organismQuantity 
             df['organismQuantity'] = df.apply(lambda x: x.individualCount if x.individualCount else x.organismQuantity, axis = 1)
             # df.apply(lambda x: f'{x.year}-{x.month:02}-{x.day:02} {x.hour:02}:{x.minute:02}', axis = 1)
@@ -120,20 +129,6 @@ for url in url_list:
             sci_names = matching_flow(sci_names)
             df = df.drop(columns=['taxonID'], errors='ignore')
             match_taxon_id = sci_names
-            # taxon_list = list(sci_names[sci_names.taxonID!=''].taxonID.unique()) + list(sci_names[sci_names.parentTaxonID!=''].parentTaxonID.unique())
-            # taxon_list = list(sci_names[sci_names.taxonID!=''].taxonID.unique())
-            # final_taxon = taxon[taxon.taxonID.isin(taxon_list)]
-            # final_taxon = pd.DataFrame(final_taxon)
-            # if len(final_taxon):
-            #     match_taxon_id = sci_names.merge(final_taxon)
-            #     # 若沒有taxonID的 改以parentTaxonID串
-            #     # match_parent_taxon_id = sci_names.drop(columns=['taxonID']).merge(final_taxon,left_on='parentTaxonID',right_on='taxonID')
-            #     # match_parent_taxon_id['taxonID'] = ''
-            #     # match_taxon_id = pd.concat([match_taxon_id, match_parent_taxon_id], ignore_index=True)
-            #     # 如果都沒有對到 要再加回來
-            #     match_taxon_id = pd.concat([match_taxon_id,sci_names[~sci_names.sci_index.isin(match_taxon_id.sci_index.to_list())]], ignore_index=True)
-            #     match_taxon_id = match_taxon_id.replace({nan: ''})
-            #     match_taxon_id[sci_cols] = match_taxon_id[sci_cols].replace({'': '-999999'})
             if len(match_taxon_id):
                 match_taxon_id = match_taxon_id.replace({nan: ''})
                 match_taxon_id[sci_cols] = match_taxon_id[sci_cols].replace({'': '-999999'})
@@ -156,14 +151,14 @@ for url in url_list:
             df['basisOfRecord'] = df['basisOfRecord'].apply(lambda x: control_basis_of_record(x))
             # dataGeneralizations 已標準化
             # 經緯度
-            df['grid_1'] = '-1_-1'
-            df['grid_5'] = '-1_-1'
-            df['grid_10'] = '-1_-1'
-            df['grid_100'] = '-1_-1'
+            # df['grid_1'] = '-1_-1'
+            # df['grid_5'] = '-1_-1'
+            # df['grid_10'] = '-1_-1'
+            # df['grid_100'] = '-1_-1'
             df['id'] = ''
-            df['standardLongitude'] = None
-            df['standardLatitude'] = None
-            df['location_rpt'] = None
+            # df['standardLongitude'] = None
+            # df['standardLatitude'] = None
+            # df['location_rpt'] = None
             for i in df.index:
                 # 先給新的tbiaID，但如果原本就有tbiaID則沿用舊的
                 df.loc[i,'id'] = str(bson.objectid.ObjectId())
@@ -175,48 +170,77 @@ for url in url_list:
                 # 座標模糊化
                 try:
                     coordinatePrecision = float(row.coordinatePrecision)
+                    df.loc[i,'dataGeneralizations'] = True
                 except:
                     coordinatePrecision = None
-                if (row.dataGeneralizations and coordinatePrecision) or row.sensitiveCategory in ['輕度','重度','縣市','座標不開放','物種不開放']:
-                    fuzzy_lon = None
-                    fuzzy_lat = None
-                    standardRawLon, standardRawLat, raw_location_rpt = standardize_coor(row.verbatimLongitude, row.verbatimLatitude)
-                    if standardRawLon and standardRawLat:
-                        df.loc[i, 'verbatimRawLatitude'] = float(row.verbatimLatitude)
-                        df.loc[i, 'verbatimRawLongitude'] = float(row.verbatimLongitude)
-                        df.loc[i, 'raw_location_rpt'] = raw_location_rpt
-                        df.loc[i, 'standardRawLongitude'] = standardRawLon
-                        df.loc[i, 'standardRawLatitude'] = standardRawLat
-                    if not coordinatePrecision and row.sensitiveCategory == '輕度':
-                        coordinatePrecision = 0.01
-                    elif not coordinatePrecision and row.sensitiveCategory == '重度':
-                        coordinatePrecision = 0.1
-                    if coordinatePrecision and row.sensitiveCategory not in ['縣市','座標不開放','物種不開放']:
-                        ten_times = math.pow(10, len(str(coordinatePrecision).split('.')[-1]))
-                        if row.verbatimLongitude:
-                            fuzzy_lon = math.floor(float(row.verbatimLongitude)*ten_times)/ten_times
-                        if row.verbatimLatitude:
-                            fuzzy_lat = math.floor(float(row.verbatimLatitude)*ten_times)/ten_times
-                        df.loc[i, 'coordinatePrecision'] = coordinatePrecision
+                is_hidden = False # 座標是否完全屏蔽
+                # if (row.dataGeneralizations and coordinatePrecision) or row.sensitiveCategory in ['輕度','重度','縣市','座標不開放','物種不開放']:
+                #     fuzzy_lon = None
+                #     fuzzy_lat = None
+                #     standardRawLon, standardRawLat, raw_location_rpt = standardize_coor(row.verbatimLongitude, row.verbatimLatitude)
+                #     if standardRawLon and standardRawLat:
+                #         df.loc[i, 'verbatimRawLatitude'] = float(row.verbatimLatitude)
+                #         df.loc[i, 'verbatimRawLongitude'] = float(row.verbatimLongitude)
+                #         df.loc[i, 'raw_location_rpt'] = raw_location_rpt
+                #         df.loc[i, 'standardRawLongitude'] = standardRawLon
+                #         df.loc[i, 'standardRawLatitude'] = standardRawLat
+                if not coordinatePrecision and row.sensitiveCategory == '輕度':
+                    coordinatePrecision = 0.01
+                    df.loc[i,'dataGeneralizations'] = True
+                elif not coordinatePrecision and row.sensitiveCategory == '重度':
+                    coordinatePrecision = 0.1
+                    df.loc[i,'dataGeneralizations'] = True
+                # if row.sensitiveCategory not in ['縣市','座標不開放','分類群不開放']:
+                #     is_hidden = True
+                #     df.loc[i,'dataGeneralizations'] = True
+                if row.sensitiveCategory in ['縣市','座標不開放','分類群不開放']:
+                    is_hidden = True
+                    df.loc[i,'dataGeneralizations'] = True
+                grid_data = create_blurred_grid_data(verbatimLongitude=row.verbatimLongitude, verbatimLatitude=row.verbatimLatitude, coordinatePrecision=coordinatePrecision, is_full_hidden=is_hidden)
+                df.loc[i,'standardRawLongitude'] = grid_data.get('standardRawLon')
+                df.loc[i,'standardRawLatitude'] = grid_data.get('standardRawLat')
+                df.loc[i,'raw_location_rpt'] = grid_data.get('raw_location_rpt')
+                df.loc[i,'standardLongitude'] = grid_data.get('standardLon')
+                df.loc[i,'standardLatitude'] = grid_data.get('standardLat')
+                df.loc[i,'location_rpt'] = grid_data.get('location_rpt')
+                df.loc[i, 'grid_1'] = grid_data.get('grid_1')
+                df.loc[i, 'grid_1_blurred'] = grid_data.get('grid_1_blurred')
+                df.loc[i, 'grid_5'] = grid_data.get('grid_5')
+                df.loc[i, 'grid_5_blurred'] = grid_data.get('grid_5_blurred')
+                df.loc[i, 'grid_10'] = grid_data.get('grid_10')
+                df.loc[i, 'grid_10_blurred'] = grid_data.get('grid_10_blurred')
+                df.loc[i, 'grid_100'] = grid_data.get('grid_100')
+                df.loc[i, 'grid_100_blurred'] = grid_data.get('grid_100_blurred')
+                # TODO 這邊要考慮是不是本來就要完全屏蔽 不然有可能是無法轉換座標 就必須要顯示原始座標
+                if grid_data.get('standardLon') or is_hidden:
+                    df.loc[i, 'verbatimLongitude'] = grid_data.get('standardLon')
+                if grid_data.get('standardLat') or is_hidden:
+                    df.loc[i, 'verbatimLatitude'] = grid_data.get('standardLat')
+                        # ten_times = math.pow(10, len(str(coordinatePrecision).split('.')[-1]))
+                        # if row.verbatimLongitude:
+                        #     fuzzy_lon = math.floor(float(row.verbatimLongitude)*ten_times)/ten_times
+                        # if row.verbatimLatitude:
+                        #     fuzzy_lat = math.floor(float(row.verbatimLatitude)*ten_times)/ten_times
+                        # df.loc[i, 'coordinatePrecision'] = coordinatePrecision
                     # elif row.sensitiveCategory in ['縣市','座標不開放','物種不開放']:
                     #     fuzzy_lon = None
                     #     fuzzy_lat = None
-                    df.loc[i, 'verbatimLongitude'] = fuzzy_lon
-                    df.loc[i, 'verbatimLatitude'] = fuzzy_lat  
-                    row = df.loc[i]
-                standardLon, standardLat, location_rpt = standardize_coor(row.verbatimLongitude, row.verbatimLatitude)
-                if standardLon and standardLat:
-                    df.loc[i,'standardLongitude'] = standardLon
-                    df.loc[i,'standardLatitude'] = standardLat
-                    df.loc[i,'location_rpt'] = location_rpt
-                    grid_x, grid_y = convert_coor_to_grid(standardLon, standardLat, 0.01)
-                    df.loc[i, 'grid_1'] = str(int(grid_x)) + '_' + str(int(grid_y))
-                    grid_x, grid_y = convert_coor_to_grid(standardLon, standardLat, 0.05)
-                    df.loc[i, 'grid_5'] = str(int(grid_x)) + '_' + str(int(grid_y))
-                    grid_x, grid_y = convert_coor_to_grid(standardLon, standardLat, 0.1)
-                    df.loc[i, 'grid_10'] = str(int(grid_x)) + '_' + str(int(grid_y))
-                    grid_x, grid_y = convert_coor_to_grid(standardLon, standardLat, 1)
-                    df.loc[i, 'grid_100'] = str(int(grid_x)) + '_' + str(int(grid_y))
+                #     df.loc[i, 'verbatimLongitude'] = fuzzy_lon
+                #     df.loc[i, 'verbatimLatitude'] = fuzzy_lat  
+                #     row = df.loc[i]
+                # standardLon, standardLat, location_rpt = standardize_coor(row.verbatimLongitude, row.verbatimLatitude)
+                # if standardLon and standardLat:
+                #     df.loc[i,'standardLongitude'] = standardLon
+                #     df.loc[i,'standardLatitude'] = standardLat
+                #     df.loc[i,'location_rpt'] = location_rpt
+                #     grid_x, grid_y = convert_coor_to_grid(standardLon, standardLat, 0.01)
+                #     df.loc[i, 'grid_1'] = str(int(grid_x)) + '_' + str(int(grid_y))
+                #     grid_x, grid_y = convert_coor_to_grid(standardLon, standardLat, 0.05)
+                #     df.loc[i, 'grid_5'] = str(int(grid_x)) + '_' + str(int(grid_y))
+                #     grid_x, grid_y = convert_coor_to_grid(standardLon, standardLat, 0.1)
+                #     df.loc[i, 'grid_10'] = str(int(grid_x)) + '_' + str(int(grid_y))
+                #     grid_x, grid_y = convert_coor_to_grid(standardLon, standardLat, 1)
+                #     df.loc[i, 'grid_100'] = str(int(grid_x)) + '_' + str(int(grid_y))
             # 資料集
             ds_name = df[['datasetName','recordType']].drop_duplicates().to_dict(orient='records')
             update_dataset_key(ds_name=ds_name, rights_holder=rights_holder)
@@ -226,12 +250,6 @@ for url in url_list:
             existed_records = pd.DataFrame(columns=['tbiaID', 'occurrenceID','datasetName'])
             existed_records = get_existed_records(df['occurrenceID'].to_list(), rights_holder)
             existed_records = existed_records.replace({nan:''})
-            # with db.begin() as conn:
-            #     qry = sa.text("""select "tbiaID", "occurrenceID", "created" from records  
-            #                     where "rightsHolder" = '{}' AND "occurrenceID" IN {}  """.format(rights_holder, str(df.occurrenceID.to_list()).replace('[','(').replace(']',')')) )
-            #     resultset = conn.execute(qry)
-            #     results = resultset.mappings().all()
-            #     existed_records = pd.DataFrame(results)
             if len(existed_records):
                 df =  df.merge(existed_records,on=["occurrenceID","datasetName"], how='left')
                 df = df.replace({nan: None})
@@ -261,6 +279,9 @@ for url in url_list:
                     if_exists='append',
                     index=False,
                     method=records_upsert)
+        # 成功之後 更新update_update_version 也有可能這批page 沒有資料 一樣從下一個c開始
+        update_update_version(update_version=update_version, rights_holder=rights_holder, current_page=c, note=url_index)
+    url_index += 1
 
 
 # 刪除is_deleted的records & match_log
@@ -268,5 +289,13 @@ delete_records(rights_holder=rights_holder,group=group,update_version=int(update
 
 # 打包match_log
 zip_match_log(group=group,info_id=info_id)
+
+# 更新update_version
+update_update_version(is_finished=True, update_version=update_version, rights_holder=rights_holder)
+
+# 更新 datahub - dataset
+# 前面已經處理過新增了 最後只需要處理deprecated的部分
+update_dataset_deprecated(rights_holder=rights_holder)
+
 
 print('done!')
