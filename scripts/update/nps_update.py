9cbe11641c537e6cbc79a701e3693b477fd92c43
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2024-04-29 09:43:47 +0800

    update

--- scripts/update/nps_update.py
+++ scripts/update/nps_update.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv(override=True)
 
-from scripts.taxon.match_taibif_utils import matching_flow
+from scripts.taxon.match_utils import matching_flow
 from scripts.utils import *
 
 
@@ -95,10 +95,9 @@ for p in range(current_page,total_page,10):
             data += result.get('Data')
     df = pd.DataFrame(data)
     # 如果 'sourceScientificName','sourceVernacularName' 都是空值才排除
-    df = df.replace({nan: '', 'NA': '', '-99999': '', None: ''})
+    df = df.replace({nan: '', None: '', 'NA': '', '-99999': '', 'N/A': ''})
     df = df.rename(columns={'basicOfRecord': 'basisOfRecord', 'created': 'sourceCreated', 'modified': 'sourceModified', 
                             'scientificName': 'sourceScientificName', 'isPreferredName': 'sourceVernacularName', 'taxonRank': 'sourceTaxonRank'})
-    # df = df.replace({nan: '', None: ''})
     df = df[~((df.sourceScientificName=='')&(df.sourceVernacularName==''))]
     # df = df[~(df.isPreferredName.isin([nan,'',None])&df.scientificName.isin([nan,'',None]))]
     if 'sensitiveCategory' in df.keys():
@@ -106,7 +105,7 @@ for p in range(current_page,total_page,10):
     # 排除重複資料集
     df = df[~df.datasetName.isin(duplicated_dataset_list)]
     if 'license' in df.keys():
-        df = df[df.license!='']
+        df = df[(df.license!='')&(~df.license.str.contains('BY NC ND|BY-NC-ND',regex=True))]
     else:
         df = []
     media_rule_list = []
@@ -190,7 +189,9 @@ for p in range(current_page,total_page,10):
                 df.loc[i, 'verbatimLatitude'] = grid_data.get('standardLat')
         # 資料集
         ds_name = df[['datasetName','recordType']].drop_duplicates().to_dict(orient='records')
-        update_dataset_key(ds_name=ds_name, rights_holder=rights_holder, update_version=update_version)
+        # return tbiaDatasetID 並加上去
+        return_dataset_id = update_dataset_key(ds_name=ds_name, rights_holder=rights_holder, update_version=update_version)
+        df = df.merge(return_dataset_id)
         # 更新match_log
         # 更新資料
         df['occurrenceID'] = df['occurrenceID'].astype('str')
@@ -207,14 +208,14 @@ for p in range(current_page,total_page,10):
             # df = df.drop(columns=['tbiaID','created_y','created_x'])
             df = df.drop(columns=['tbiaID'])
         # match_log要用更新的
-        match_log = df[['occurrenceID','id','sourceScientificName','taxonID','match_higher_taxon','match_stage','stage_1','stage_2','stage_3','stage_4','stage_5','group','rightsHolder','created','modified']]
+        match_log = df[['occurrenceID','id','sourceScientificName','taxonID','match_higher_taxon','match_stage','stage_1','stage_2','stage_3','stage_4','stage_5','stage_6','stage_7','stage_8','group','rightsHolder','created','modified']]
         match_log = match_log.reset_index(drop=True)
         match_log = update_match_log(match_log=match_log, now=now)
         match_log.to_csv(f'/portal/media/match_log/{group}_{info_id}_{p}.csv',index=None)
         # records要用更新的
         # 已經串回原本的tbiaID，可以用tbiaID做更新
         df['is_deleted'] = False
-        df = df.drop(columns=['match_stage','stage_1','stage_2','stage_3','stage_4','stage_5','taxon_name_id','sci_index'],errors='ignore')
+        df = df.drop(columns=['match_stage','stage_1','stage_2','stage_3','stage_4','stage_5','stage_6','stage_7','stage_8','taxon_name_id','sci_index', 'datasetURL','gbifDatasetID', 'gbifID'],errors='ignore')
         # 最後再一起匯出
         # # 在solr裡 要使用id當作名稱 而非tbiaID
         # df.to_csv(f'/solr/csvs/updated/{group}_{info_id}_{p}.csv', index=False)
@@ -243,7 +244,11 @@ zip_match_log(group=group,info_id=info_id)
 update_update_version(is_finished=True, update_version=update_version, rights_holder=rights_holder)
 
 # 更新 datahub - dataset
-# 前面已經處理過新增了 最後只需要處理deprecated的部分
+# update if deprecated
 update_dataset_deprecated(rights_holder=rights_holder, update_version=update_version)
+
+# update dataset info
+update_dataset_info(rights_holder=rights_holder)
+
 
 print('done!')
