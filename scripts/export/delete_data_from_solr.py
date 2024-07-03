f523928beab8a607e44efc6d6f2f2e19dc29a2d3
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2024-07-03 11:29:39 +0800

    init commit

--- scripts/export/delete_data_from_solr.py
+++ scripts/export/delete_data_from_solr.py
@@ -1,40 +1,38 @@
+
+
 import subprocess
 import pandas as pd
-from app import db
-from sqlalchemy import text
-import sqlalchemy as sa
-
-
-deleted_time = ''
-
-with db.begin() as conn:
-    qry = sa.text('''SELECT * FROM deleted_records WHERE deleted > '{}'; '''.format(deleted_time))
-    resultset = conn.execute(qry)
-    results = resultset.mappings().all()
-
-
 import math
 import json
 
-total_page = math.ceil(len(results) / 100)
+
+# TODO 這邊要修改成每次的deleted資料
+df = pd.read_csv('deleted_2024-06-20.csv')
+
+total_page = math.ceil(len(df) / 100)
 limit = 100
+
+df = df.drop(columns=['id'])
+df['deleted'] = df['deleted'].apply(lambda x: x.split('.')[0])
+df = df.rename(columns={'tbiaID': 'id'})
+
+results = df.to_dict('records')
 
 print('total_page', total_page)
 for p in range(0,total_page):
     print(p)
     deleting_rs = results[p*limit:(p+1)*limit]
-    deleting_ids = [d['tbiaID'] for d in deleting_rs]
+    deleting_ids = [d['id'] for d in deleting_rs]
     deleting_str = ' OR '.join(deleting_ids)
     commands = f''' curl http://solr:8983/solr/tbia_records/update/?commit=true -H "Content-Type: text/xml" --data-binary '<delete><query>id:({deleting_str})</query></delete>'; ''' 
     process = subprocess.Popen(commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     a = process.communicate()
     # 移到 deleted_records
-    deleting_df = pd.DataFrame(deleting_rs)
-    deleting_df['deleted'] = deleting_df['deleted'].dt.strftime('%Y-%m-%d %H:%M:%S')
-    deleting_dict = deleting_df.to_dict('records')
-    deleting_dict = json.dumps(deleting_dict)
+    # deleting_df = pd.DataFrame(deleting_rs)
+    # deleting_df = df[]
+    # deleting_df['deleted'] = deleting_df['deleted'].apply(lambda x: x.split('.')[0])
+    # deleting_dict = deleting_df.to_dict('records')
+    deleting_dict = json.dumps(deleting_rs)
     commands = f''' curl http://solr:8983/solr/deleted_records/update/?commit=true -H "Content-Type: application/json" --data-binary '{deleting_dict}'; ''' 
     process = subprocess.Popen(commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     a = process.communicate()
-
-
